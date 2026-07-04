30051cbc39227e5d55b5dfc9e1eeb5cd9f1113fa
TusharSarap <tusharsarap87@gmail.com>
2026-07-04 22:05:34 +0530

    commrnt 1

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+inline char fn(static char a){
+    cout<<"overload1"<<endl;
+    a=++a;
+    return a;
+    }
+
+int main()
+{
+
+    cout<<fn('4')<<endl; 
+    return 0;
+}
