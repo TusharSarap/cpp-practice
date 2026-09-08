dd83fe0e3c9eb2caaf29e9c7b6c669d6c7cf71c9
TusharSarap <tusharsarap87@gmail.com>
2026-09-08 10:49:42 +0530

    Add parentheses balancing check program
    Implement a program to check balanced parentheses using a stack.

---
+++ check_parentheses.cpp
@@ -0,0 +1,41 @@
+// Write a program to check whether the parentheses in a given string
+// are balanced or not using a stack.
+// Input: A string containing '(' and ')' characters.
+// Output:
+// "You have Equal perenthesis closing" if all parentheses are balanced,
+// otherwise "You dont have Equal perenthesis closing".
+
+#include <iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    string s;
+    stack<char> check;
+    cin>>s;
+    
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='('){
+            check.push(s[i]);
+        }
+        else if(s[i]==')'){
+            check.pop();
+        }
+        // cout<<check.top();
+    }
+    
+    if(check.empty()){
+        cout<<"You have Equal perenthesis closing";
+    }
+    else{
+        cout<<"You dont have Equal perenthesis closing";
+    }
+    
+    
+    
+    
+
+    return 0;
+}
